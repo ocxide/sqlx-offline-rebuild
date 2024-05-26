eee52524ba3709d54a9d8ed9efb8b44eea718000
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-05-26 09:44:07 -0500

    feat: set cli about

--- src/cli.rs
+++ src/cli.rs
@@ -3,9 +3,9 @@ use std::path::PathBuf;
 use clap::{command, Parser};
 
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
+#[command(author, version, about, long_about = None, version)]
 pub struct Args {
-    /// Target directory
+    /// Target directory where the .sqlx directory is located
     pub target: PathBuf,
 }
 
